06a0fb4881ac9d07bebee3a74d780445138ac460
Kevin Lewis <kevin.lewis@vonage.com>
2020-12-21 20:17:49 +0000

    Netlify function to update avatar

---
+++ functions/update.js
@@ -0,0 +1,26 @@
+const fetch  = require('node-fetch')
+const Twitter = require('twitter')
+const twitter = new Twitter({
+    consumer_key: process.env.TWITTER_KEY,
+    consumer_secret: process.env.TWITTER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+exports.handler = async event => {
+  try {
+    let gradients = await fetch('https://gitcdn.link/repo/ghosh/uiGradients/master/gradients.json').then(r => r.json())
+    let gradient = gradients[Math.floor(Math.random() * gradients.length)].name.toLowerCase().split(' ').join('-').split('&').join('-').split("'").join('')
+
+    const url = `${process.env.URL}/avatars/${gradient}.png`;
+    const b64 = await fetch(url).then(r => r.buffer()).then(buf => buf.toString('base64'));
+    twitter.post("account/update_profile_image", { image: b64 })
+    return res({ message: 'ok' })
+  } catch (error) {
+    return res({ error }, 500)
+  }
+}
+
+function res(o, statusCode = 200) {
+  return { statusCode, body: JSON.stringify(o) }
+}
